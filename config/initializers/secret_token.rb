1b9fbb15dfb4ee3a81da240499c68000160d81e2
Jorge Lobo <jorgelobo04@gmail.com>
2013-08-10 14:19:30 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Danca::Application.config.secret_key_base = 'd4466286a40e3e9722010db0dfff8a20a4337a4e8d68e7348fa2b4eac3ecf537dcec63373c4ac8d05e91f7d60be846c8323bad699c99f2406f85b06a29cbaefa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Danca::Application.config.session_store :cookie_store, key: '_danca_session'
